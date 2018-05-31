9fc244bd32a0bf0b3d37c90609e5150aa690c6ae
user1 <>
2018-05-31 08:19:14 +0200

    implement operating mode

---
+++ flash-chip-proxy.rb
@@ -0,0 +1,17 @@
+class FlashChipProxy
+
+  attr_reader(:mode)
+  def mode=(operating_mode)
+    if (operating_mode == :learn || operating_mode == :replace)
+      @mode = operating_mode
+    else
+      raise "operating_mode has to be :learn or :replace, was " +
+            operating_mode.inspect()
+    end
+  end
+  
+  def initialize()
+    self.mode = :replace
+  end
+
+end
---
+++ test-flash-chip-proxy.rb
@@ -0,0 +1,21 @@
+require_relative "flash-chip-proxy"
+require "test/unit"
+
+class TestFlashChipProxy < Test::Unit::TestCase
+
+  def test_operating_modes
+    # the FlashChipProxy can be in modes :learn or :replace
+
+    # Default is :replace
+    assert_equal(:replace, FlashChipProxy.new().mode() )
+
+    # Mode :learn has to be enabled
+    p = FlashChipProxy.new()
+    p.mode = :learn
+    assert_equal(:learn, p.mode())
+
+    # We cannot assign arbitrary modes
+    assert_raise(RuntimeError){p.mode = :hello}
+  end
+ FlashChipProxy
+end
