1f3d892292b34d95d21c853bfff21ab03836778e
user1 <>
2018-05-31 11:47:43 +0200

    implementation of learning

--- flash-chip-proxy.rb
+++ flash-chip-proxy.rb
@@ -13,6 +13,12 @@ class FlashChipProxySettings
     end
   end
 
+  def learn_from(remote_host, remote_port = 80)
+    self.mode=:learn
+    @remote_host = remote_host
+    @remote_port = remote_port
+  end
+  
   attr_reader :storage_directory
   def storage_directory=(directory)
     if File.directory?(directory)
@@ -66,9 +72,26 @@ class FlashChipProxy < FlashChipProxySettings
     connection.write(IO.read(cache_file))
   end
 
-  def serve_from_remote_and_store(connection, request, cache_file)
+  def serve_from_remote_and_store(client, request, cache_file)
+    server = TCPSocket.open(@remote_host, @remote_port)
+    server.write(request)
+    if File.exist(cache_file)
+      File.rename(cache_file, cache_file + ".old")
+    end
+    file = File.open(cache_file, "w")
+    data = ""
+    begin
+      loop do
+        data = server.sysread(1000)
+        client.write(data)
+        file.write(data)
+        data = ""
+      end
+    rescue EOFError, SystemCallError
+    end
+    server.close()
+    file.close()
   end
-
   
   def read_request(connection)
     request = ""
